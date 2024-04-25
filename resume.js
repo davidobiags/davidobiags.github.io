b94dca953ec22a465d8b34c3d98672d5a3cec740
davidobiags <168042484+davidobiags@users.noreply.github.com>
2024-04-24 23:52:48 -0600

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,14 @@
+function dimBrightness() {
+    if (body.style.backgroundColor = "white") {
+        body.style.backgroundColor = "#f2f2f2";
+        let newText = document.getElementsByTagName(button).value;
+        newText = "Bright";
+
+    }
+    else {
+        body.style.backgroundColor = "#f2f2f2";
+        let newText = document.getElementsByTagName(button).value;
+        newText = "Dim";
+
+    }
+}    
